9fdbdfe0e854e21a8736196a6d9b02c71c7d0616
nhattruongniit <nhattruongniit@yahoo.com.vn>
2018-09-02 01:22:31 +0700

    cut page index for misty

--- misty-frontend/dist/js/main.js
+++ misty-frontend/dist/js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
+
+    /* TODO: jquery slimscroll for cart ============*/
     $('.js-cart-slimscroll').slimScroll({
-        height: '370px'
+        height: '310px',
+        size: '5px'
+    });
+
+    $('.js-banner').slick({
+        dots: false,
+        autoplay: false,
+        autoplaySpeed: 5000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        prevArrow: '<div class="c-button__slider c-button__slider--prev"><i class="fa fa-long-arrow-left"></i></div>',
+        nextArrow: '<div class="c-button__slider c-button__slider--next"><i class="fa fa-long-arrow-right"></i></div>'
     });
 })
